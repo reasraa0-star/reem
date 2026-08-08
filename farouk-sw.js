ec79e2290ec91b24a009d622d5e456394b105b19
reasraa0-star <reasraa0@gmail.com>
2026-08-08 14:19:03 +0300

    Add files via upload

--- farouk-sw.js
+++ farouk-sw.js
@@ -1,4 +1,4 @@
-var C='farouk-v2';
+var C='farouk-v3';
 self.addEventListener('install',function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(['./farouk.html','./farouk-manifest.json'])}).then(function(){return self.skipWaiting()}))});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.filter(function(x){return x!==C}).map(function(x){return caches.delete(x)}))}).then(function(){return self.clients.claim()}))});
 self.addEventListener('fetch',function(e){if(e.request.method!=='GET')return;var u=new URL(e.request.url);if(u.origin!==self.location.origin)return;if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(function(r){var cp=r.clone();caches.open(C).then(function(c){c.put(e.request,cp)});return r}).catch(function(){return caches.match(e.request)}));return}e.respondWith(caches.match(e.request).then(function(h){return h||fetch(e.request).then(function(r){var cp=r.clone();caches.open(C).then(function(c){c.put(e.request,cp)});return r})}))});
